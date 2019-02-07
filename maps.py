eb6fe09fab449e7ab9400a41a3d28518d0545b24
Sofiia2001 <sophi.tatosh@gmail.com>
2019-02-08 00:14:52 +0200

    Second layer *start*

--- maps.py
+++ maps.py
@@ -37,37 +37,58 @@ def location_coordinates(location):
     return coordinates
 
 
-def marker_color():
-    pass
-
-
 def place_check(place, data):
-    i = 0
-    for place in places:
+    for i in range(len(data)):
         if place in data[i][2].lower():
             return True
-        i += 1
         if i == len(data):
             break
 
 
-def map_formation(year, data, places):
+def counting(places, data):
+    counter_lst = [[place, 0] for place in places if place_check(place, data)]
+    for line in data:
+        for el in counter_lst:
+            if el[0] in line[2].lower():
+                el[1] += 1
+    return counter_lst
+
+
+
+def filling_colour(counted, colours_list):
+    counted.sort(key = lambda num: num[1])
+    for lst in counted:
+        lst.append(colours_list[counted.index(lst)])
+        lst.append(colours_list[len(colours_list) - counted.index(lst) - 1])
+    return counted
+
+
+def map_formation(year, data, colours_list):
     map = folium.Map(location = [49.817545, 24.023932],
                      zoom_start = 3)
 
-    featuregroup = folium.FeatureGroup(name = 'Location by year')
+    fg_loc = folium.FeatureGroup(name = 'Location by year')
+    # fg_color = folium.FeatureGroup(name = 'Colored by amount of movies in a country')
+    tooltip = 'Click me!'
 
     for line in tqdm(data):
-        for place in places:
-            if year in line and place_check(place, data):
+        for place in colours_list:
+            if year in line and place_check(place[0], data):
                 try:
-                    featuregroup.add_child(folium.Marker(location = location_coordinates(line[2]),
+                    fg_loc.add_child(folium.Marker(location = location_coordinates(line[2]),
                                             popup = year + ' year\n' + line[0],
-                                            icon = folium.Icon()))
+                                            icon = folium.Icon(color = place[3]),
+                                                   tooltip = tooltip).add_to(map))
                 except:
                     pass
 
-    map.add_child(featuregroup)
+                # fg_color.add_child(folium.Choropleth(geo_data = 'loc.list',
+                                                    # fill_color = place[2]))
+
+
+    map.add_child(fg_loc)
+    # map.add_child(fg_color)
+    # map.add_child(folium.LayerControl())
     map.save('FilmMap.html')
 
 
@@ -75,9 +96,22 @@ if __name__ == '__main__':
     places = []
     year = str(input('Please, input a year you want to get the location of movies from: '))
     num = int(input('Enter the number of places you want to compare (at least two): '))
+
     while num > 0:
-        place = str(input('Please, now enter those places with enters (city/country): '))
+        place = str(input('Please, now enter those countries with enters: '))
         places.append(place.lower())
         num -= 1
+
+    colours_list = ['red', 'blue', 'green', 'purple', 'orange', 'darkred',
+         'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
+         'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen']
+         # 'gray', 'black', 'lightgray']
     data = reading_from_file('loc.list', year)
-    map_formation(year, data, places)
+    counted = counting(places, data)
+    colours_list = filling_colour(counted, colours_list)
+    map_formation(year, data, colours_list)
+
+#зробити винятки вводу інфи користувачем
+# зафарбована карта за к-кстю фільмів на території(окрема функція з порівняннями) (зробити БЕЗ списку кольорів)
+# другий шар
+# придумати третій
